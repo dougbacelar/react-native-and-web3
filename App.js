c58d3639e61276142196a55d37de1486a83bbe30
Jordi Moraleda <jordi@tvrbo.pro>
2018-11-27 18:22:28 +0100

    Using expo 30 and providing with a complete workflow

--- App.js
+++ App.js
@@ -1,23 +1,47 @@
+import './global';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import './global';
-
+import { AES, lib, enc } from "crypto-js"
 const Web3 = require('web3');
+const bip39 = require("bip39")
+const hdKey = require("ethereumjs-wallet/hdkey")
 
 const web3 = new Web3(
   new Web3.providers.HttpProvider('https://mainnet.infura.io/'),
 );
+// web3.eth.getBlock('latest').then(console.log);
+
+// TODO: NOT RANDOM ENOUGH
+const entropy = lib.WordArray.random(24).toString()
+
+const mnemonic = bip39.entropyToMnemonic(entropy)
+const encryptedMnemonic = AES.encrypt(mnemonic, "my encryption key here").toString()
+const decryptedMnemonic = AES.decrypt(encryptedMnemonic, 'my encryption key here').toString(enc.Utf8)
+const hdwallet = hdKey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic))
+const wallet = hdwallet.derivePath(`m/44'/60'/0'/0/0`).getWallet()
+const data = new Date().toJSON()
+const signature = web3.eth.accounts.sign(data, wallet.getPrivateKeyString()).messageHash
 
 export default class App extends React.Component {
   render() {
-    // printing on the console the latest ethereum block
-    web3.eth.getBlock('latest').then(console.log);
-
     return (
       <View style={styles.container}>
-        <Text>Check your console!</Text>
-        <Text>You should find info on the latest ethereum block.</Text>
+        <Text>{"\nMNEMONIC"}</Text>
+        <Text>{mnemonic}</Text>
+        <Text>{"\nENCRYPTED"}</Text>
+        <Text>{encryptedMnemonic}</Text>
+        <Text>{"\nDECRYPTED"}</Text>
+        <Text>{decryptedMnemonic}</Text>
+        <Text>{"\nWALLET PrivKey"}</Text>
+        <Text>{wallet.getPrivateKeyString()}</Text>
+        <Text>{"\nWALLET PubKey"}</Text>
+        <Text>{wallet.getAddressString()}</Text>
+        <Text>{"\nDATA"}</Text>
+        <Text>{data}</Text>
+        <Text>{"\nSIGNED DATA"}</Text>
+        <Text>{signature}</Text>
       </View>
     );
   }
@@ -26,8 +50,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
   },
 });
