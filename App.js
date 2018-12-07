32ecea3a1acabe31f847cc32e52444f9f379ff9a
Jordi <ledfusion@users.noreply.github.com>
2018-12-07 16:48:37 +0100

    Replace ethereumjs-wallet to solve a binary crash on the Android standalone

--- App.js
+++ App.js
@@ -1,58 +1,66 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
 import './global';
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-import { AES, lib, enc } from "crypto-js"
+const { AES, lib, enc } = require("crypto-js")
 const Web3 = require('web3');
 const bip39 = require("bip39")
-const hdKey = require("ethereumjs-wallet/hdkey")
+const ethers = require("ethers")
 
 const web3 = new Web3(
   new Web3.providers.HttpProvider('https://mainnet.infura.io/'),
 );
-// web3.eth.getBlock('latest').then(console.log);
-
-// TODO: NOT RANDOM ENOUGH
-const entropy = lib.WordArray.random(24).toString()
-
-const mnemonic = bip39.entropyToMnemonic(entropy)
-const encryptedMnemonic = AES.encrypt(mnemonic, "my encryption key here").toString()
-const decryptedMnemonic = AES.decrypt(encryptedMnemonic, 'my encryption key here').toString(enc.Utf8)
-const hdwallet = hdKey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic))
-const wallet = hdwallet.derivePath(`m/44'/60'/0'/0/0`).getWallet()
-const data = new Date().toJSON()
-const signature = web3.eth.accounts.sign(data, wallet.getPrivateKeyString()).messageHash
 
 export default class App extends React.Component {
+  state = {}
+  componentDidMount() {
+    // web3.eth.getBlock('latest').then(console.log);
+
+    // TODO: NOT RANDOM ENOUGH
+    const entropy = lib.WordArray.random(24).toString()
+
+    const mnemonic = bip39.entropyToMnemonic(entropy)
+    const encryptedMnemonic = AES.encrypt(mnemonic, "my encryption key here").toString()
+    const decryptedMnemonic = AES.decrypt(encryptedMnemonic, 'my encryption key here').toString(enc.Utf8)
+
+    const data = new Date().toJSON()
+
+    const hd = ethers.utils.HDNode.fromMnemonic(mnemonic)
+    const wallet = hd.derivePath(`m/44'/60'/0'/0/0`)
+    const signature = web3.eth.accounts.sign(data, wallet.privateKey).messageHash
+
+    this.setState({ mnemonic, encryptedMnemonic, decryptedMnemonic, privateKey: wallet.privateKey, address: wallet.address.toLowerCase(), data, signature })
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{"\nMNEMONIC"}</Text>
-        <Text>{mnemonic}</Text>
-        <Text>{"\nENCRYPTED"}</Text>
-        <Text>{encryptedMnemonic}</Text>
-        <Text>{"\nDECRYPTED"}</Text>
-        <Text>{decryptedMnemonic}</Text>
-        <Text>{"\nWALLET PrivKey"}</Text>
-        <Text>{wallet.getPrivateKeyString()}</Text>
-        <Text>{"\nWALLET PubKey"}</Text>
-        <Text>{wallet.getAddressString()}</Text>
-        <Text>{"\nDATA"}</Text>
-        <Text>{data}</Text>
-        <Text>{"\nSIGNED DATA"}</Text>
-        <Text>{signature}</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.view}>
+          <Text>{"\nMNEMONIC"}</Text>
+          <Text>{this.state.mnemonic}</Text>
+          <Text>{"\nENCRYPTED"}</Text>
+          <Text>{this.state.encryptedMnemonic}</Text>
+          <Text>{"\nDECRYPTED"}</Text>
+          <Text>{this.state.decryptedMnemonic}</Text>
+          <Text>{"\nWALLET PrivKey"}</Text>
+          <Text>{this.state.privateKey}</Text>
+          <Text>{"\nWALLET @"}</Text>
+          <Text>{this.state.address}</Text>
+          <Text>{"\nDATA"}</Text>
+          <Text>{this.state.data}</Text>
+          <Text>{"\nSIGNED DATA"}</Text>
+          <Text>{this.state.signature}</Text>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    margin: 20,
     backgroundColor: '#fff',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
   },
+  view: {
+    margin: 30
+  }
 });
